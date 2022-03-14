8cdaa2ca04897d693cc6073b4d2733c6df5aedbf
Manas12Singh <89171896+Manas12Singh@users.noreply.github.com>
2022-03-14 22:01:25 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,20 +3,20 @@ import tkinter as tk
 
 global l6
 
-Window=tk.Tk()
-Window.title("Dashboard")
+Window2=tk.Tk()
+Window2.title("Dashboard")
 
 def student_detail():
-    Window.destroy()
+    Window2.destroy()
     exec(open("SD.py").read())
 def fee_detail():
-    Window.destroy()
+    Window2.destroy()
     exec(open("Fee.py").read())
 def att_detail():
-    Window.destroy()
+    Window2.destroy()
     exec(open("Attendance.py").read())
 def aca_detail():
-    Window.destroy()
+    Window2.destroy()
     exec(open("Academic.py").read())
 def blue(n):
     l6.config(fg='#0000FF')
@@ -24,7 +24,7 @@ def black(n):
     l6.config(fg='#000000')
 def login(n):
     exec(open("Login.py").read())
-    Window.destroy()
+    Window2.destroy()
   
 
 df=pd.read_excel("SD.xlsx")
@@ -36,38 +36,38 @@ for i in range(len(df)):
         bc=df.iat[i,3]
     
 
-l2=tk.Label(Window,text=n,font=("Serif","40"))
+l2=tk.Label(Window2,text=n,font=("Serif","40"))
 l2.grid(row=1,columnspan=3)
-l2=tk.Label(Window,text="STUDENT ID: "+sid,font=("Serif","20"))
+l2=tk.Label(Window2,text="STUDENT ID: "+sid,font=("Serif","20"))
 l2.grid(row=2,columnspan=3)
-l2=tk.Label(Window,text="BATCH CODE: "+bc,font=("Serif","20"))
+l2=tk.Label(Window2,text="BATCH CODE: "+bc,font=("Serif","20"))
 l2.grid(row=3,columnspan=3)
-l2=tk.Label(Window,text=" ",font=("Serif","40"))
+l2=tk.Label(Window2,text=" ",font=("Serif","40"))
 l2.grid(row=4,columnspan=3)
 
-b1=tk.Button(Window,text="STUDENT DETAILS",font=("Serif","15"),command=student_detail)
+b1=tk.Button(Window2,text="STUDENT DETAILS",font=("Serif","15"),command=student_detail)
 b1.grid(row=5,column=0)
 
-l3=tk.Label(Window,text="        ")
+l3=tk.Label(Window2,text="        ")
 l3.grid(row=5,column=1)
 
-b2=tk.Button(Window,text="FEE DETAILS",font=("Serif","15"),command=fee_detail)
+b2=tk.Button(Window2,text="FEE DETAILS",font=("Serif","15"),command=fee_detail)
 b2.grid(row=5,column=2)
 
-l4=tk.Label(Window,text=" ",font=("Serif","20"))
+l4=tk.Label(Window2,text=" ",font=("Serif","20"))
 l4.grid(row=6,columnspan=3)
 
 
-b2=tk.Button(Window,text="ATTENDANCE DETAILS",font=("Serif","15"),command=att_detail)
+b2=tk.Button(Window2,text="ATTENDANCE DETAILS",font=("Serif","15"),command=att_detail)
 b2.grid(row=7,column=0)
 
-b2=tk.Button(Window,text="ACADEMIC DETAILS",font=("Serif","15"),command=aca_detail)
+b2=tk.Button(Window2,text="ACADEMIC DETAILS",font=("Serif","15"),command=aca_detail)
 b2.grid(row=7,column=2)
 
-l5=tk.Label(Window,text=" ",font=("Serif","20"))
+l5=tk.Label(Window2,text=" ",font=("Serif","20"))
 l5.grid(row=6,columnspan=3)
 
-l6=tk.Label(Window,text="Back to home",font='Serif 10 underline')
+l6=tk.Label(Window2,text="Back to home",font='Serif 10 underline')
 l6.grid(row=9,column=1)
 
 l6.bind('<Enter>',blue)
