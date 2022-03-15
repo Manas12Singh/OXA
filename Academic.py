e98a4a1599758b86474e28e8538d16da12edb5ef
Manas12Singh <89171896+Manas12Singh@users.noreply.github.com>
2022-03-15 06:54:18 +0530

    Update Academic.py

--- Academic.py
+++ Academic.py
@@ -10,13 +10,11 @@ Window3.title("Academic Details")
 df1=pd.read_excel("SD.xlsx")
 df2=pd.read_excel("SD.xlsx", sheet_name=2)
 
-for i in range(len(df1)):
-    id=df1.iat[i,0]
-    if sid==id:
-        n=df1.iat[i,1]
-        l=df2.iat[i,[1,2,3]]
-        break
-
+l1=df2.iat[i,[1,2,3]]
+l2=df2.iat[i,[5,6,7]]
+l3=df2.iat[i,[8,9,10]]
+l4=df2.iat[i,[12,13,14]]
+        
 l1=tk.Label(Window3,text="Academic Details",font=("Serif","40"))
 l1.grid(row=0,columnspan=3)
 
