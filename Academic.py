aa857138e668332687f63bd6f2e29fef72983440
Manas12Singh <89171896+Manas12Singh@users.noreply.github.com>
2022-03-15 16:24:57 +0530

    Update Academic.py

--- Academic.py
+++ Academic.py
@@ -10,10 +10,7 @@ Window3.title("Academic Details")
 df1=pd.read_excel("SD.xlsx")
 df2=pd.read_excel("SD.xlsx", sheet_name=2)
 
-l1=df2.iat[i,[1,2,3]]
-l2=df2.iat[i,[5,6,7]]
-l3=df2.iat[i,[8,9,10]]
-l4=df2.iat[i,[12,13,14]]
+l1=df2.iloc[i,1:]
         
 l1=tk.Label(Window3,text="Academic Details",font=("Serif","40"))
 l1.grid(row=0,columnspan=3)
