b901a4c8b5607954dc7e4be9d317f66bfd5dc52a
Manas12Singh <89171896+Manas12Singh@users.noreply.github.com>
2022-03-15 16:35:50 +0530

    Update Academic.py

--- Academic.py
+++ Academic.py
@@ -20,4 +20,10 @@ l2.grid(row=1,columnspan=3)
 l3=tk.Label(Window3,text="Examination: ", font=("Serif","13"))
 l3.grid(row=2,column=0)
 
-Wondow3.mainloop()
+n = tk.StringVar()
+c1=tk.Combobox(Window3,textvariable=n)
+c1.grid(row=2,column=1)
+c1['values']=['Term 1','Term 2','Term 3','Term 4']
+c1.current(1)
+
+Window3.mainloop()
